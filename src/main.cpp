e42518f3c64d8d0325f89b09baa37147f7e2eec8
Doug Fajardo <doug@fajardo.hm>
2024-04-09 17:43:22 -0400

    adding kinematics, notes about sounds, update README

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,19 @@
 #include <Arduino.h>
 #include "CmdProcessor.h"
-#include <Servos.h>
+#include "Servos.h"
+#include "Kinematics.h"
 
-// put function declarations here:
-int myFunction(int, int);
-CmdProcessor *commands=nullptr;
+CmdProcessor *commands;
+Kinematics kin;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(119200); 
   commands = new CmdProcessor(&Serial);  // Commands from serial input...
+  kin = Kinematics();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   commands->loop();
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
